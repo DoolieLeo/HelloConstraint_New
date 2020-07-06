01963df00eda8d9204a81d629d790be744c6feca
DoolieLeo <leoqwe123456@gmail.com>
2020-07-06 16:07:46 +0800

    helo dooile

---
+++ app/src/main/java/com/example/HelloConstraint/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.HelloConstraint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount= (TextView)findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view){
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view){
+        ++mCount;
+        if(mShowCount!=null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
+
+        if(mCount%2==0)
+        {
+            view.setBackgroundColor(Color.GREEN);
+        }
+        else
+        {
+            view.setBackgroundColor(Color.BLUE);
+        }
+
+        if(mCount!=0)
+        {
+            findViewById(R.id.zero_button).setBackgroundColor(Color.RED);
+        }
+
+    }
+    public void CoutIt(View view)//zero
+    {
+        mCount=0;
+        mShowCount.setText(Integer.toString(mCount));
+        view.setBackgroundColor(Color.GRAY);
+
+    }
+}
